2052a4fe04b2a3726f6e7c3b903f762edbda933f
Abdullah Al Suad <suadabdullahal@gmail.com>
2024-09-28 16:22:21 +0600

    first commit

---
+++ inc/templates/nav.php
@@ -0,0 +1,5 @@
+<p>
+    <a href="/php_program/crud/index.php?task=report">All Students</a> |
+    <a href="/php_program/crud/index.php?task=add">Add New Student</a> |
+    <a href="/php_program/crud/index.php?task=seed">Seed</a>
+</p>
